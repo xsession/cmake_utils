03c4c4cd485480ebb4d2a3c24217c86c2e43667c
Ivanyi Laszlo <k9eqto@gmail.com>
2022-10-26 22:10:36 +0200

    Add directory walk through example

--- cmake_project_generator/cmake_project_generator.py
+++ cmake_project_generator/cmake_project_generator.py
@@ -18,6 +18,7 @@ Options:
     --debug-on
     --generate-toolchain-file
     --generate-run-file
+    --print-file-tree
 '''
 
 import logging
@@ -26,10 +27,23 @@ import os
 from pathlib import Path
 from docopt import docopt
 
+class CMakeProjectGenerator:
+    def __init__(self) -> None:
+        pass
+
 if __name__ == '__main__':
     arguments = docopt(__doc__, version="CMake Project Generator v0.0.1")
     # print(arguments)
 
     if arguments['--package-folders']:
         print(str(arguments['--package-folders']))
+
+    if arguments['--print-file-tree']:
+        print(str(arguments['--print-file-tree']))
+        for dirpath, dirnames,filenames in os.walk(r'C:\Users\Yoga\OneDrive\Desktop\GIT\chsm\sm_gen'):
+            print(
+                f"Root:{dirpath}\n"
+                f"Sub-directories: {dirnames}\n"
+                f"Files: {filenames}\n"
+            )
 
