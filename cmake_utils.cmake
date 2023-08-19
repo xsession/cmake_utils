33d3469aac35b98f6eab8b0702ed7a2892ab0995
Ivanyi Laszlo <k9eqto@gmail.com>
2023-08-19 18:24:05 +0200

    Update cmake_utils.cmake

--- cmake_utils.cmake
+++ cmake_utils.cmake
@@ -1,17 +1,22 @@
 cmake_minimum_required(VERSION 3.25)
 
-########################  Set where to search .cmake files  ############################ 
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/utils")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/output_generators")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/IDE_project_generators")
-message("CMAKE_MODULE_PATH: -> ${CMAKE_MODULE_PATH}")
+if(NOT DEFINED CMAKE_UTILS_INCLUDED)
 
-########################  External .cmake files includes  ############################ 
-include(GNUInstallDirs)
-include(CMakePackageConfigHelpers)
-include(diagnostic)
-include(add_targets)
-include(add_projects)
-include(graphviz)
-include(path_list)
+    ########################  Set where to search .cmake files  ############################ 
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/utils")
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/output_generators")
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/IDE_project_generators")
+    message("CMAKE_MODULE_PATH: -> ${CMAKE_MODULE_PATH}")
+
+    ########################  External .cmake files includes  ############################ 
+    include(GNUInstallDirs)
+    include(CMakePackageConfigHelpers)
+    include(diagnostic)
+    include(add_targets)
+    include(graphviz)
+    include(path_list)
+
+    set(CMAKE_UTILS_INCLUDED TRUE)
+
+endif()
